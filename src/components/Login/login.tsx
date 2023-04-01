7b911efcf2932b81884e44b1890518d6af10d363
eleven-ding <1559298665@qq.com>
2023-04-02 00:44:21 +0800

    fix: 解决多次发送请求的问题

--- src/components/Login/login.tsx
+++ src/components/Login/login.tsx
@@ -30,46 +30,49 @@ export default function Login() {
       }
       const { link } = LoginTypeList.find((item) => item.type === key)!;
       openLoginWindow(link, "loginWindow");
-      async function handlePostMessage({ data }: any) {
-        const { type, data: payload } = data;
-        if (!type || !payload) {
-          return;
-        }
-        try {
-          messageApi.info({
-            content: "登陆中，请稍后",
-            duration: 1,
-          });
-          startGlobalLoading();
-          const { data, code } = await login(payload);
-          if (code === -1) {
-            throw new Error("登录失败");
-          }
-
-          const { token, userInfo } = data;
-          messageApi.success({
-            content: `你好，${userInfo.username}，欢迎您的到来`,
-            duration: 2,
-          });
-          localStorage.setItem("token", token);
-          localStorage.setItem("userInfo", JSON.stringify(userInfo));
-          setUserInfo(userInfo);
-        } catch (error) {
-          messageApi.warning({
-            content: (error as Error).message,
-            duration: 2,
-          });
-        } finally {
-          closeGlobalLoading();
-        }
-      }
-      window.addEventListener("message", handlePostMessage, false);
-      return () => {
-        window.removeEventListener("message", handlePostMessage, false);
-      };
     },
     [messageApi]
   );
+
+  useEffect(() => {
+    async function handlePostMessage({ data }: any) {
+      const { type, data: payload } = data;
+      if (!type || !payload) {
+        return;
+      }
+      try {
+        messageApi.info({
+          content: "登陆中，请稍后",
+          duration: 1,
+        });
+        startGlobalLoading();
+        const { data, code } = await login(payload);
+        if (code === -1) {
+          throw new Error("登录失败");
+        }
+
+        const { token, userInfo } = data;
+        messageApi.success({
+          content: `你好，${userInfo.username}，欢迎您的到来`,
+          duration: 2,
+        });
+        localStorage.setItem("token", token);
+        localStorage.setItem("userInfo", JSON.stringify(userInfo));
+        setUserInfo(userInfo);
+      } catch (error) {
+        messageApi.warning({
+          content: (error as Error).message,
+          duration: 2,
+        });
+      } finally {
+        closeGlobalLoading();
+      }
+    }
+    window.addEventListener("message", handlePostMessage, false);
+    return () => {
+      window.removeEventListener("message", handlePostMessage, false);
+    };
+  }, [messageApi]);
 
   // 重新进入浏览器保持登录
   useEffect(() => {
