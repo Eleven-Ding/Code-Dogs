0df9db64b92775ec5e5d96d680b7845655720369
eleven-ding <1559298665@qq.com>
2023-04-02 20:02:18 +0800

    feat: 允许自定义分页数量;

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ export type PostListResponse = {
   count: number;
   rows: PostItemType[];
 };
-const pageSize = 6;
+const limit = 8;
 let serverFetch = true;
 export default function Home(props: AppProps & { data: PostListResponse }) {
   const {
@@ -31,9 +31,13 @@ export default function Home(props: AppProps & { data: PostListResponse }) {
   const [page, updatePage] = useState(1);
   const [pageList, setPageList] = useState(rows);
   const [total, setTotal] = useState(count);
+  const [pageSize, setPageSize] = useState(limit);
 
   const handlePageSizeChange = useCallback((page: number) => {
     updatePage(page);
+  }, []);
+  const handleShowSizeChange = useCallback((current: number, size: number) => {
+    setPageSize(size);
   }, []);
 
   useEffect(() => {
@@ -58,7 +62,7 @@ export default function Home(props: AppProps & { data: PostListResponse }) {
       .finally(() => {
         // TODO: 关闭 loading
       });
-  }, [page]);
+  }, [page, pageSize]);
   return (
     <>
       <PostsList count={total} rows={pageList}></PostsList>
@@ -68,6 +72,9 @@ export default function Home(props: AppProps & { data: PostListResponse }) {
         pageSize={pageSize}
         current={page}
         onChange={handlePageSizeChange}
+        responsive
+        onShowSizeChange={handleShowSizeChange}
+        pageSizeOptions={[8, 12, 20]}
       ></Pagination>
     </>
   );
@@ -78,7 +85,7 @@ export async function getServerSideProps(context: GetServerSideProps) {
   try {
     const result = await getPostsList({
       offset: 0,
-      limit: pageSize,
+      limit,
     });
     propsData = result.data;
   } catch (error) {}
