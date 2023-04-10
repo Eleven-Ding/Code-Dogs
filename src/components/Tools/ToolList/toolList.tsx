467f76fc41359442c7e7488ca63dc2d09843dae6
eleven-ding <1559298665@qq.com>
2023-04-10 23:21:57 +0800

    feat: 添加tools部分列表

---
+++ src/components/Tools/ToolList/toolList.tsx
@@ -0,0 +1,60 @@
+import styles from "./toolList.module.scss";
+
+export interface ToolItem {
+  toolName: string;
+  icon: string;
+  link: string;
+}
+const tools: ToolItem[] = [
+  {
+    toolName: "图床",
+    icon: "icon-yunshangchuan_o",
+    link: "",
+  },
+  {
+    toolName: "图片转Base64",
+    icon: "icon-zhuanhuantuxing",
+    link: "",
+  },
+  {
+    toolName: "图片压缩",
+    icon: "icon-yasuobao",
+    link: "",
+  },
+  {
+    toolName: "时间戳转换",
+    icon: "icon-shijianchuo",
+    link: "",
+  },
+  {
+    toolName: "自动生成正则",
+    icon: "icon-regularExpression-o",
+    link: "",
+  },
+
+  {
+    toolName: "IP地址获取",
+    icon: "icon-ip",
+    link: "",
+  },
+  {
+    toolName: "自动部署",
+    icon: "icon-liuchengbushu",
+    link: "",
+  },
+];
+
+export function ToolList() {
+  return (
+    <div className={styles["tool-list"]}>
+      {tools.map((tool, index) => {
+        return (
+          <div key={index} className={styles["tool-item"]}>
+            <span className={["iconfont", tool.icon].join(" ")}></span>
+            <span>{tool.toolName}</span>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- src/pages/tools.tsx
+++ src/pages/tools.tsx
@@ -1,8 +1,8 @@
+import { ToolList } from "@/components/Tools/ToolList/toolList";
 export default function Tools() {
   return (
-    <>
-      <h1>常用工具集合</h1>
-      <h1>正在开发中，一些自己常用的工具，免得在网上去找</h1>
-    </>
+    <div>
+      <ToolList />
+    </div>
   );
 }
