f0950cad85db81d89f59886d4c1e646a7cf65691
eleven-ding <1559298665@qq.com>
2023-04-11 23:28:49 +0800

    chore: Footer 组件动效

--- src/common/Footer/footer.tsx
+++ src/common/Footer/footer.tsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from "react";
 import styles from "./footer.module.scss";
+import { getCurrentFormatTime } from "@/utils/getCurrentTime";
+
 export default function Footer() {
+  const [passTime, updatePassTime] = useState("");
+  useEffect(() => {
+    const timer = setInterval(() => {
+      updatePassTime(getCurrentFormatTime());
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
   return (
     <div className={styles["footer-container"]}>
       <p>本系统由 Next + Express 联合驱动</p>
@@ -7,8 +19,11 @@ export default function Footer() {
         <span className={styles["storage"]}>COS对象存储</span>蜀ICP备20019999-2{" "}
         <span className={styles["hosting"]}>托管于阿里云</span>
       </p>
-      <p>本站已苟且偷生 882天14时42分41秒</p>
-      <p>DingShiYi‘s Blog</p>
+      <p>本站已苟且偷生 {passTime}</p>
+      <p>
+        DingShiYi‘s Blog{" "}
+        <span className={styles["foot-dancing"]}>ｸﾞｯ!(๑•̀ㅂ•́)و✧</span>
+      </p>
     </div>
   );
 }
