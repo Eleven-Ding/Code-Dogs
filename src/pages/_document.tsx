5c0661b794d0da2e5a80a7aa7920891097a66d07
eleven-ding <1559298665@qq.com>
2023-04-28 21:22:18 +0800

    chore: 添加 chrome 广告

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1163321291228195"
+          crossOrigin="anonymous"
+        ></Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
-        <script>0</script> 
+        <script>0</script>
       </body>
     </Html>
-  )
+  );
 }
